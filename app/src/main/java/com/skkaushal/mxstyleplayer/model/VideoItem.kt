91f8079d820229c32af08704ab9f8a9e8239474f
skgdrive932 <skgdrive932@gmail.com>
2026-09-22 13:20:00 +0530

    Update VideoItem.kt

--- app/src/main/java/com/skkaushal/mxstyleplayer/model/VideoItem.kt
+++ app/src/main/java/com/skkaushal/mxstyleplayer/model/VideoItem.kt
@@ -1,2 +1,9 @@
 package com.skkaushal.mxstyleplayer.model
-data class VideoItem(val name:String,val uri:String,val duration:Long,val size:Long)
+
+data class VideoItem(
+    val name: String,
+    val uri: String,
+    val duration: Long,
+    val size: Long,
+    val folderName: String = "Internal Storage"
+)
