3e50244a95e98566f9619d38d98bd3427fb97295
skgdrive932 <skgdrive932@gmail.com>
2026-09-22 21:32:17 +0530

    Create AudioItem.kt

---
+++ app/src/main/java/com/skkaushal/mxstyleplayer/model/AudioItem.kt
@@ -0,0 +1,11 @@
+package com.skkaushal.mxstyleplayer.model
+
+import android.net.Uri
+
+data class AudioItem(
+    val id: Long,
+    val title: String,
+    val artist: String,
+    val duration: Long,
+    val uri: Uri
+)
