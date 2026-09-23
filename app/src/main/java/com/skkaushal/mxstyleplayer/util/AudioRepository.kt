afd80b67fb20cbf3afa3f0355b6e5799901cf197
skgdrive932 <skgdrive932@gmail.com>
2026-09-23 22:51:15 +0530

    Update AudioRepository.kt

--- app/src/main/java/com/skkaushal/mxstyleplayer/util/AudioRepository.kt
+++ app/src/main/java/com/skkaushal/mxstyleplayer/util/AudioRepository.kt
@@ -7,48 +7,50 @@ import com.skkaushal.mxstyleplayer.model.AudioItem
 
 class AudioRepository(private val context: Context) {
 
-    fun getAllAudios(): List<AudioItem> {
-        val audioList = mutableListOf<AudioItem>()
+    fun getAllAudioTracks(): List<AudioItem> {
+        val audioList = ArrayList<AudioItem>()
         val projection = arrayOf(
             MediaStore.Audio.Media._ID,
             MediaStore.Audio.Media.TITLE,
             MediaStore.Audio.Media.ARTIST,
             MediaStore.Audio.Media.ALBUM,
-            MediaStore.Audio.Media.DURATION,
-            MediaStore.Audio.Media.DATA
+            MediaStore.Audio.Media.DURATION
         )
 
         val cursor = context.contentResolver.query(
             MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
             projection,
-            "${MediaStore.Audio.Media.IS_MUSIC} != 0",
+            MediaStore.Audio.Media.IS_MUSIC + "!= 0",
             null,
             "${MediaStore.Audio.Media.TITLE} ASC"
         )
 
-        cursor?.use { c ->
-            val idCol = c.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
-            val titleCol = c.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE)
-            val artistCol = c.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST)
-            val albumCol = c.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM)
-            val durationCol = c.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)
-            val dataCol = c.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA)
+        cursor?.use {
+            val idCol = it.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
+            val titleCol = it.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE)
+            val artistCol = it.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST)
+            val albumCol = it.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM)
+            val durationCol = it.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)
 
-            while (c.moveToNext()) {
-                val id = c.getLong(idCol)
-                val title = c.getString(titleCol) ?: "Unknown"
-                val artist = c.getString(artistCol) ?: "Unknown Artist"
-                val album = c.getString(albumCol) ?: "Unknown Album"
-                val duration = c.getLong(durationCol)
-                val path = c.getString(dataCol) ?: ""
-                
-                val folderName = path.substringBeforeLast("/").substringAfterLast("/")
+            while (it.moveToNext()) {
+                val id = it.getLong(idCol)
+                val title = it.getString(titleCol) ?: "Unknown Song"
+                val artist = it.getString(artistCol) ?: "<Unknown Artist>"
+                val album = it.getString(albumCol) ?: "Unknown Album"
+                val duration = it.getLong(durationCol)
+                val uri = ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, id)
 
-                val contentUri = ContentUris.withAppendedId(
-                    MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, id
+                // Corrected parameter sequence matching AudioItem data class
+                audioList.add(
+                    AudioItem(
+                        id = id,
+                        title = title,
+                        artist = artist,
+                        album = album,
+                        duration = duration,
+                        uri = uri
+                    )
                 )
-
-                audioList.add(AudioItem(id, title, artist, album, folderName, duration, contentUri))
             }
         }
         return audioList
