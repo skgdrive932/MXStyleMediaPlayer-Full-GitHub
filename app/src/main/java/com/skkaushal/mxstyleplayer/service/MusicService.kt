93fd4eae295edbd6a36c692d9f6357aabc6d7d5a
skgdrive932 <skgdrive932@gmail.com>
2026-09-23 15:06:05 +0530

    Create MusicService.kt

---
+++ app/src/main/java/com/skkaushal/mxstyleplayer/service/MusicService.kt
@@ -0,0 +1,44 @@
+package com.skkaushal.mxstyleplayer.service
+
+import androidx.media3.common.AudioAttributes
+import androidx.media3.common.C
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaSession
+import androidx.media3.session.MediaSessionService
+
+class MusicService : MediaSessionService() {
+
+    private var player: ExoPlayer? = null
+    private var mediaSession: MediaSession? = null
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val audioAttributes = AudioAttributes.Builder()
+            .setUsage(C.USAGE_MEDIA)
+            .setContentType(C.AUDIO_CONTENT_TYPE_MUSIC)
+            .build()
+
+        player = ExoPlayer.Builder(this)
+            .setAudioAttributes(audioAttributes, true)
+            .setHandleAudioBecomingNoisy(true)
+            .build()
+
+        player?.let { exoPlayer ->
+            mediaSession = MediaSession.Builder(this, exoPlayer).build()
+        }
+    }
+
+    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? {
+        return mediaSession
+    }
+
+    override fun onDestroy() {
+        mediaSession?.run {
+            player.release()
+            release()
+            mediaSession = null
+        }
+        super.onDestroy()
+    }
+}
