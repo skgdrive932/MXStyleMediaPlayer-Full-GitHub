b93af27873a978577c333b84416817ad94c8451b
skgdrive932 <skgdrive932@gmail.com>
2026-09-23 23:26:58 +0530

    Update FolderItem.kt

--- app/src/main/java/com/skkaushal/mxstyleplayer/model/FolderItem.kt
+++ app/src/main/java/com/skkaushal/mxstyleplayer/model/FolderItem.kt
@@ -1,8 +1,6 @@
 package com.skkaushal.mxstyleplayer.model
 
-import java.io.Serializable
-
 data class FolderItem(
     val folderName: String,
-    val videoList: ArrayList<VideoItem>
-) : Serializable
+    val videoList: List<VideoItem>
+)
