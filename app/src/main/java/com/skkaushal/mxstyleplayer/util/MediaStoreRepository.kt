bd8f8c7dd15a5aba86b38900b3fae64687881791
skgdrive932 <skgdrive932@gmail.com>
2026-09-23 22:43:48 +0530

    Update MediaStoreRepository.kt

--- app/src/main/java/com/skkaushal/mxstyleplayer/util/MediaStoreRepository.kt
+++ app/src/main/java/com/skkaushal/mxstyleplayer/util/MediaStoreRepository.kt
@@ -3,6 +3,7 @@ package com.skkaushal.mxstyleplayer.util
 import android.content.ContentUris
 import android.content.Context
 import android.provider.MediaStore
+import com.skkaushal.mxstyleplayer.model.AudioItem
 import com.skkaushal.mxstyleplayer.model.FolderItem
 import com.skkaushal.mxstyleplayer.model.VideoItem
 import java.util.Locale
@@ -56,6 +57,45 @@ class MediaStoreRepository(private val context: Context) {
             folderList.add(FolderItem(name, videos))
         }
         return folderList
+    }
+
+    fun getAllAudioTracks(): List<AudioItem> {
+        val audioList = ArrayList<AudioItem>()
+        val projection = arrayOf(
+            MediaStore.Audio.Media._ID,
+            MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.ARTIST,
+            MediaStore.Audio.Media.ALBUM,
+            MediaStore.Audio.Media.DURATION
+        )
+
+        val cursor = context.contentResolver.query(
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            MediaStore.Audio.Media.IS_MUSIC + "!= 0",
+            null,
+            "${MediaStore.Audio.Media.TITLE} ASC"
+        )
+
+        cursor?.use {
+            val idCol = it.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
+            val titleCol = it.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE)
+            val artistCol = it.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST)
+            val albumCol = it.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM)
+            val durationCol = it.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)
+
+            while (it.moveToNext()) {
+                val id = it.getLong(idCol)
+                val title = it.getString(titleCol) ?: "Unknown Song"
+                val artist = it.getString(artistCol) ?: "<Unknown Artist>"
+                val album = it.getString(albumCol) ?: "Unknown Album"
+                val duration = it.getLong(durationCol)
+                val uri = ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, id)
+
+                audioList.add(AudioItem(id, title, artist, album, duration, uri))
+            }
+        }
+        return audioList
     }
 
     private fun formatDuration(durationMs: Long): String {
