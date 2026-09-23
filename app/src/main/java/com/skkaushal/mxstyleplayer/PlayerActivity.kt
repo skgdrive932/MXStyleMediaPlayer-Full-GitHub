eaae3b401e1d7626e7edd70cbb23917d77b50409
skgdrive932 <skgdrive932@gmail.com>
2026-09-23 16:11:57 +0530

    Update PlayerActivity.kt

--- app/src/main/java/com/skkaushal/mxstyleplayer/PlayerActivity.kt
+++ app/src/main/java/com/skkaushal/mxstyleplayer/PlayerActivity.kt
@@ -1,63 +1,18 @@
 package com.skkaushal.mxstyleplayer
 
-import android.content.pm.ActivityInfo
-import android.net.Uri
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.media3.common.MediaItem
-import androidx.media3.exoplayer.ExoPlayer
-import com.skkaushal.mxstyleplayer.databinding.ActivityPlayerBinding
+import com.skkaushal.mxstyleplayer.model.VideoItem
 
 class PlayerActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityPlayerBinding
-    private var player: ExoPlayer? = null
-    private var isLandscape = false
+    companion object {
+        var videoList: ArrayList<VideoItem> = ArrayList()
+        var currentPosition: Int = 0
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityPlayerBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val videoUriString = intent.getStringExtra("VIDEO_URI")
-        if (videoUriString != null) {
-            initializePlayer(Uri.parse(videoUriString))
-        }
-
-        setupRotateButton()
-    }
-
-    private fun initializePlayer(uri: Uri) {
-        player = ExoPlayer.Builder(this).build().also { exoPlayer ->
-            binding.playerView.player = exoPlayer
-            val mediaItem = MediaItem.fromUri(uri)
-            exoPlayer.setMediaItem(mediaItem)
-            exoPlayer.prepare()
-            exoPlayer.playWhenReady = true
-        }
-    }
-
-    private fun setupRotateButton() {
-        // PlayerView par click karke screen rotation toggle karne ka clean tarika
-        binding.playerView.setFullscreenButtonClickListener { isFullscreen ->
-            requestedOrientation = if (isFullscreen) {
-                ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE
-            } else {
-                ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-            }
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        releasePlayer()
-    }
-
-    private fun releasePlayer() {
-        player?.let { exoPlayer ->
-            exoPlayer.release()
-        }
-        player = null
+        setContentView(R.layout.activity_player)
     }
 }
