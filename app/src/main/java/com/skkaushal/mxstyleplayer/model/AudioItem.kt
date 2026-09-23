89572a3f5d3440634e46f9b8abfbdb564ce46175
skgdrive932 <skgdrive932@gmail.com>
2026-09-23 23:16:03 +0530

    Update AudioItem.kt

--- app/src/main/java/com/skkaushal/mxstyleplayer/model/AudioItem.kt
+++ app/src/main/java/com/skkaushal/mxstyleplayer/model/AudioItem.kt
@@ -1,12 +1,10 @@
 package com.skkaushal.mxstyleplayer.model
 
-import android.net.Uri
-
 data class AudioItem(
     val id: Long,
     val title: String,
     val artist: String,
     val album: String,
-    val duration: Long,
-    val uri: Uri
+    val dataPath: String = "",
+    val songCount: Int = 0
 )
