7565e81c4056ee13e471b7c9f824bc2392104fbc
skgdrive932 <skgdrive932@gmail.com>
2026-09-26 16:23:30 +0530

    Update VideoFoldersFragment.kt

--- app/src/main/java/com/skkaushal/mxstyleplayer/VideoFoldersFragment.kt
+++ app/src/main/java/com/skkaushal/mxstyleplayer/VideoFoldersFragment.kt
@@ -8,12 +8,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.skkaushal.mxstyleplayer.model.VideoItem
 import com.skkaushal.mxstyleplayer.util.MediaStoreRepository
 
 class VideoFoldersFragment : Fragment() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var repository: MediaStoreRepository
+    private lateinit var adapter: VideoAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,24 +24,27 @@ class VideoFoldersFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_video_folders, container, false)
 
-        recyclerView = view.findViewById(R.id.recyclerViewFolders)
+        val idRv = resources.getIdentifier("recyclerViewVideoFolders", "id", requireContext().packageName)
+        recyclerView = if (idRv != 0) view.findViewById(idRv) else view.findViewById(resources.getIdentifier("recyclerViewVideo", "id", requireContext().packageName))
+
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
-
         repository = MediaStoreRepository(requireContext())
-        loadFolders()
 
-        return view
-    }
+        val videos = try {
+            repository.getVideos()
+        } catch (e: Exception) {
+            emptyList<VideoItem>()
+        }
 
-    private fun loadFolders() {
-        val folderList = repository.getAllFolders()
-        val adapter = FolderAdapter(folderList) { folderItem ->
-            val intent = Intent(requireContext(), FolderVideosActivity::class.java).apply {
-                putExtra("FOLDER_NAME", folderItem.folderName)
+        adapter = VideoAdapter(videos) { videoItem, _ ->
+            val intent = Intent(requireContext(), VideoPlayerActivity::class.java).apply {
+                putExtra("VIDEO_URI", videoItem.path)
+                putExtra("VIDEO_TITLE", videoItem.title)
             }
-            FolderVideosActivity.videoList = folderItem.videoList
             startActivity(intent)
         }
+
         recyclerView.adapter = adapter
+        return view
     }
 }
